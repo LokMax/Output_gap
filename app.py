a90faecb6ce026cce16203e02d224f49c0fe17ae
LokMax <118130671+LokMax@users.noreply.github.com>
2025-08-06 13:05:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,50 +34,56 @@ def x13_seasonal_adjustment(series, freq='Q'):
         return pd.Series(res.seasonal.values, index=series.index.to_timestamp())
 
 def calc_output_gap(df_capital, df_population, df_other):
-    """Расчёт разрыва выпуска"""
-    # Датафреймы
+    # --- подготовка данных ---
     df_capital['date'] = pd.to_datetime(df_capital['date'])
     df_population['date'] = pd.to_datetime(df_population['date'])
     df_other['date'] = pd.to_datetime(df_other['date'])
 
-    # Фактический выпуск
+    # --- фактический выпуск ---
     output = df_other[df_other['indicator'] == "Приведённая отгрузка"].sort_values('date')
     output_sa = x13_seasonal_adjustment(output.set_index('date')['value'], freq='Q')
 
-    # Инвестиции
+    # --- инвестиции ---
     investments = df_other[df_other['indicator'] == "Приведённые инвестиции в основной капитал"].sort_values('date')
     investments_sa = x13_seasonal_adjustment(investments.set_index('date')['value'], freq='Q')
 
-    # Занятость
+    # --- занятость ---
     employment = df_other[df_other['indicator'] == "Численность работников"].sort_values('date')
 
-    # Капитал (годовой) → квартальный через накопление
+    # --- капитал ---
     capital = df_capital[df_capital['indicator'] == "Реальные основные фонды за вычетом учётного износа"].sort_values('date')
     capital_q = capital['value'].iloc[0]
-    depreciation_rate = 0.05 / 4  # фикс 5% в год
+    depreciation_rate = 0.05 / 4
     capital_inventory = []
     for invest in investments_sa:
         capital_q = capital_q * (1 - depreciation_rate) + invest
         capital_inventory.append(capital_q)
 
-    # Потенциальная занятость
+    # --- потенциальная занятость ---
     population = df_population[df_population['indicator'] == "Численность населения в трудоспособном возрасте, всего"].sort_values('date')
-    pop_q = np.repeat(population['value'].values, 4)[:len(employment)]
+
+    # Генерируем квартальный ряд населения с выравниванием
+    pop_q = np.repeat(population['value'].values, 4)
+    if len(pop_q) < len(employment):
+        pop_q = np.pad(pop_q, (0, len(employment)-len(pop_q)), mode='edge')
+    else:
+        pop_q = pop_q[:len(employment)]
+
     emp_ratio = employment['value'].values / pop_q
     _, emp_cycle = hpfilter(emp_ratio, lamb=1600)
     potential_employment = (emp_ratio - emp_cycle) * pop_q * (1 - 0.046)
 
-    # Потенциальный выпуск
+    # --- потенциальный выпуск ---
     ln_output = np.log(output['value'].values)
     ln_capital = np.log(capital_inventory)
     ln_potential_employment = np.log(potential_employment)
     ln_tfp = ln_output - CAPITAL_ELASTICITY * ln_capital - LABOUR_ELASTICITY * ln_potential_employment
-    ln_tfp_cycle, ln_tfp_trend = hpfilter(ln_tfp, lamb=1600)
+    _, ln_tfp_trend = hpfilter(ln_tfp, lamb=1600)
     ln_potential_output = ln_tfp_trend + CAPITAL_ELASTICITY * ln_capital + LABOUR_ELASTICITY * ln_potential_employment
     potential_output = np.exp(ln_potential_output)
     output_gap = output_sa.values - potential_output
 
-    df = pd.DataFrame({
+    return pd.DataFrame({
         'date': output['date'].values,
         'output': output['value'].values,
         'output_sa': output_sa.values,
@@ -88,8 +94,7 @@ def calc_output_gap(df_capital, df_population, df_other):
         'potential_output_inv': potential_output,
         'output_gap_inventory': output_gap
     })
-    return df
-
+    
 if uploaded_file:
     xls = pd.ExcelFile(uploaded_file)
     df_capital = pd.read_excel(xls, sheet_name='Capital')
